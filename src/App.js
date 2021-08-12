59b8229de0cabb451802eee6337012919f981e89
walter97 <walter97ogozaly@gmail.com>
2021-08-12 13:44:43 -0500

    BrowserRouter --> HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import NavBar from "./components/NavBar";
